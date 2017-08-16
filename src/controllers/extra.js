d77aca93f7e70c3f41fab44e457d21287e0759f9
Natansab <natansab@gmail.com>
2017-08-16 14:09:08 +0200

    score computing OK

--- src/controllers/extra.js
+++ src/controllers/extra.js
@@ -1,10 +1,4 @@
 /**
- * Dependencies
- */
-
-import mongoose from 'mongoose';
-
-/**
  * Models
  */
 
@@ -14,22 +8,33 @@ import UserModel from '../schemas/user';
  * Public
  */
 
+/**
+* Score is out of 100.
+* How the score is built?
+* # of pictures: 30pts
+* # of likes: 30pts
+* # of interests: 20pts
+* # of chats: 20pts
+* # of blocked: -100pts
+*/
+
+/* eslint no-param-reassign: ["error", { "props": true, "ignorePropertyModificationsFor": ["userDoc"] }] */
+
 async function score() {
-  /**
-   * Score is out of 100.
-   * How the score is built?
-   * Number of photos: 0 photos = 0 pts, 5 photos = 30pts
-   *
-   *
-   *
-   */  // // Score = 0 to 100.
-  // picsScore / 40
-  const userDoc = await UserModel.findById(mongoose.Type.ObjectId('5985ea9f872dbe0319ddacc7'));
+  const userDocs = await UserModel.find();
 
-  const picsScore = Math.max(30, userDoc.pictures.length * 0.2 * 30);
+  userDocs.forEach(async userDoc => {
+    const numOfLikes = await UserModel.count({ likes: { $in: [userDoc._id] } });
+    const numOfBlocked = await UserModel.count({ blocked: { $in: [userDoc._id] } });
 
-
-  return 'OK';
+    const picsScore = Math.min(30, userDoc.pictures.length * 0.2 * 30);
+    const likesScore = Math.min(30, (numOfLikes / Math.max(1, userDoc.likes.length)) * 15);
+    const interestsScore = Math.min(20, userDoc.interest.length * 4);
+    const chatsScore = 20;
+    const blockedScore = -(numOfBlocked * 10);
+    userDoc.score = Math.max(0, picsScore + likesScore + interestsScore + chatsScore + blockedScore);
+    return userDoc.save();
+  });
 }
 
 
