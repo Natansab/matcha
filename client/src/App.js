1180af1433b58159bfeb9814126a217ba32ced1e
Natansab <natansab@gmail.com>
2017-08-17 17:28:42 +0200

    init

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+        <p>
+        <button onClick=refreshScore>REFRESH SCORES</button>
+        </p>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
